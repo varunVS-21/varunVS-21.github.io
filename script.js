023cc9a90d0de2c79cbd879955f48f76cc1ea224
VARUN VS <305476729+varunVS-21@users.noreply.github.com>
2026-07-20 19:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,25 @@ function eraseEffect() {
 }
 
 typeEffect();
+// Back to Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
